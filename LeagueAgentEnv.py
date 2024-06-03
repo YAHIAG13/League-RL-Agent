4fa7d18bb351946008c1c79d4360eff1fe97b8e4
YAHIAG13 <78378644+YAHIAG13@users.noreply.github.com>
2024-06-03 14:18:54 +0100

    cleanup

--- LeagueAgentEnv.py
+++ LeagueAgentEnv.py
@@ -1,97 +1,3 @@
-# fmt: off
-"""
-Make your own custom environment
-================================
-
-This documentation overviews creating new environments and relevant
-useful wrappers, utilities and tests included in Gymnasium designed for
-the creation of new environments. You can clone gym-examples to play
-with the code that is presented here. We recommend that you use a virtual environment:
-
-.. code:: console
-
-   git clone https://github.com/Farama-Foundation/gym-examples
-   cd gym-examples
-   python -m venv .env
-   source .env/bin/activate
-   pip install -e .
-
-Subclassing gymnasium.Env
--------------------------
-
-Before learning how to create your own environment you should check out
-`the documentation of Gymnasium’s API </api/env>`__.
-
-We will be concerned with a subset of gym-examples that looks like this:
-
-.. code:: sh
-
-   gym-examples/
-     README.md
-     setup.py
-     gym_examples/
-       __init__.py
-       envs/
-         __init__.py
-         grid_world.py
-       wrappers/
-         __init__.py
-         relative_position.py
-         reacher_weighted_reward.py
-         discrete_action.py
-         clip_reward.py
-
-To illustrate the process of subclassing ``gymnasium.Env``, we will
-implement a very simplistic game, called ``GridWorldEnv``. We will write
-the code for our custom environment in
-``gym-examples/gym_examples/envs/grid_world.py``. The environment
-consists of a 2-dimensional square grid of fixed size (specified via the
-``size`` parameter during construction). The agent can move vertically
-or horizontally between grid cells in each timestep. The goal of the
-agent is to navigate to a target on the grid that has been placed
-randomly at the beginning of the episode.
-
--  Observations provide the location of the target and agent.
--  There are 4 actions in our environment, corresponding to the
-   movements “right”, “up”, “left”, and “down”.
--  A done signal is issued as soon as the agent has navigated to the
-   grid cell where the target is located.
--  Rewards are binary and sparse, meaning that the immediate reward is
-   always zero, unless the agent has reached the target, then it is 1.
-
-An episode in this environment (with ``size=5``) might look like this:
-
-where the blue dot is the agent and the red square represents the
-target.
-
-Let us look at the source code of ``GridWorldEnv`` piece by piece:
-"""
-
-# %%
-# Declaration and Initialization
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-# Our custom environment will inherit from the abstract class
-# ``gymnasium.Env``. You shouldn’t forget to add the ``metadata``
-# attribute to your class. There, you should specify the render-modes that
-# are supported by your environment (e.g. ``"human"``, ``"rgb_array"``,
-# ``"ansi"``) and the framerate at which your environment should be
-# rendered. Every environment should support ``None`` as render-mode; you
-# don’t need to add it in the metadata. In ``GridWorldEnv``, we will
-# support the modes “rgb_array” and “human” and render at 4 FPS.
-#
-# The ``__init__`` method of our environment will accept the integer
-# ``size``, that determines the size of the square grid. We will set up
-# some variables for rendering and define ``self.observation_space`` and
-# ``self.action_space``. In our case, observations should provide
-# information about the location of the agent and target on the
-# 2-dimensional grid. We will choose to represent observations in the form
-# of dictionaries with keys ``"agent"`` and ``"target"``. An observation
-# may look like ``{"agent": array([1, 0]), "target": array([0, 3])}``.
-# Since we have 4 actions in our environment (“right”, “up”, “left”,
-# “down”), we will use ``Discrete(4)`` as an action space. Here is the
-# declaration of ``GridWorldEnv`` and the implementation of ``__init__``:
-
 import numpy as np
 import pygame
 
@@ -99,7 +5,7 @@ import gymnasium as gym
 from gymnasium import spaces
 
 
-class LeagueAgent(gym.Env):
+class LeagueAgentEnv(gym.Env):
 
     def __init__(self, size, render_mode=None):
         
@@ -129,11 +35,23 @@ class LeagueAgent(gym.Env):
 
         # We have a total of 15 actions
         # 8 movement actions corresponding to "north", "south", "east", "west", and in between them.
-        # 4 ability actions for q, w, e, r :  
+        # 4 ability actions for q, w, e, r
         # two summoner spells actions d (ghost) and f (smite)
         # an action to base with b : recall(1), no_recall(0)
         # open shop and buy items with p
-        self.action_space = spaces.MultiDiscrete([9,4,3,2,2])
+        self.action_space = spaces.MultiDiscrete(
+            [
+                9,  # movement
+                2,  # q
+                2,  # w
+                2,  # e
+                2,  # r
+                2,  # d
+                2,  # f
+                2,  # b
+                2   # p
+            ]
+        )
 
         """
         The following dictionary maps abstract actions from `self.action_space` to
@@ -163,6 +81,7 @@ class LeagueAgent(gym.Env):
         }
 
     def reset(self, seed=None, options=None):
+
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
@@ -182,6 +101,7 @@ class LeagueAgent(gym.Env):
         return observation, info
 
     def step(self, action):
+        
         # Map the action (element of {0,1,2,3}) to the direction we walk in
         direction = self._action_to_direction[action]
         # We use `np.clip` to make sure we don't leave the grid
